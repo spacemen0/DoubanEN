ec5ca39c3623ba7bf88b305e8e556d5c6268a336
spacemen0 <frakctures@outlook.com>
2024-03-04 00:59:24 +0100

    improve featured Item

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,14 @@ export default function App() {
         userID: 1,
         reviewDate: '2022-03-01',
         star: 3.5,
-        content: `This song is amazing!This song is amazing!This song is amazing!
-        This song is amazing!This song is amazing!This song is amazing!This song is amazing!
-        This song is amazing!This song is amazing!This song is amazing!This song is amazing!
-        This song is amazing!This song is amazing!This song is amazing!This song is amazing!`,
+        content: `Be aggressive. Be selfish. Be as Allie X sets out to be on Girl With No Face. Nobody to prove anything to. Creativity for the indulgence of pleasure and nothing more. More artists should be open to this correspondence with what the soul desires. Girl With No Face demands nothing from its listeners because it has no intention of entertaining their expectations. It puts Allie X in a position of power. With the same roaring and upbeat focus as Future Islands’ People Who Aren’t There Anymore, the similarity and taste from Girl With No Face bring about the marrying of wild electropop and effective lyricism which on first listen sounds disconnected. Go around again and feel the flow. What a rush it makes for, and Allie X has a fine creation on their hands.  
+
+Opener Weird World begins the running trend of consistent synth wonders. Sudden and grating uses of industrialised filler and drum machine beauties create a raging core which Allie X builds on. Friends can be replaced by your thoughts and the ambitions of the mind. Those darker and momentous highs are found in Girl With No Face, the vocal range on display is remarkable and benefits from a slightly sinister, creaking notion underneath it all. Familiar faces are not the friends you think them to be, as Allie X warns. Alienate those around you and fight the resulting outrage. The sense of theatrics within Girl With No Face is all part of the Allie X severity – taken up to the next level by the drum machine and techno blitz of Off With Her Tits.  
+
+Fiddly electronics and the constant whir and buzz of these moments is what must be loved about Girl With No Face. Exceptionally unique and inspired moments of mixing that champion the new wave and synthpop blur. Many are in the thick of it though few do it as well as Allie X. Sincere yet sinister tones on John and Jonathan settle in well, the piece feeling ever-so-slightly haunted by its own shadow. Galina is about as pop adjacent as it gets, but even then the tinge of 1980s stylishness and the influence of the period flows through. Allie X is comfortable with a modernisation of the classic pop junctures of an era long gone – but within this shakeup are some of her finest songs. Punchy, energetic and lunging at the past with all the intent of a furious artist carving out a career-best effort, Girl With No Face truly has it all. 
+
+Flickers of horrifying tech love on Hardware Software create another in a series of uncomfortable yet enchanting listens for Allie X. Nothing shy of exceptional the whole way through. Slim gothic tones can be heard throughout, but it never feels like the focus Allie X sets out for. In between all those plinking lights of high-strung and hair-raising instrumentals is a desire to be left alone with your thoughts. Girl With No Face presents the point of not being able to know someone – the shapeshifter at large as Allie X redefines the path to being known for an elusive stance. Girl With No Face dances through the likes of Pet Shop Boys and New Order with their heavy drum machines but Allie X finds sharp and joyous ruminations in the dark she creates. You Slept on Me is the high. That we did. Never too late to make amends and give Girl With No Face a play.  
+`,
       },
     },
     {
--- src/components/FeaturedItem.tsx
+++ src/components/FeaturedItem.tsx
@@ -22,19 +22,19 @@ export function FeaturedItem({ image, music, review }: { image: ImageProps; musi
                 <div className="max-w-[50%]">                <Image {...image} /></div>
                 <div className="md:ml-8 flex-1 flex flex-col items-center !md:items-start justify-center">
                     <h2 className="text-xl font-semibold mt-2">{music.title}</h2>
-                    <p className="mt-2">Artist: {music.artist}</p>
-                    <p className="mt-2">Genre: {music.genre}</p>
+                    <p className="mt-2"><span className="font-semibold">Artist:&nbsp;</span> {music.artist}</p>
+                    <p className="mt-2"><span className="font-semibold">Genre:&nbsp;</span> {music.genre}</p>
                     <div className="flex items-center mt-2">
-                        <p>Rating:&nbsp;</p>
+                        <p className="font-semibold">Rating:&nbsp;</p>
                         <div className="flex ">{renderStars()}</div>
                     </div>
-                    <p className="mt-2">Average Rating: {music.average}</p>
-                    <p className="mt-2">Total Ratings: {music.ratings}</p>
+                    <p className="mt-2"><span className="text-xl font-semibold">{music.average}&nbsp;</span>
+                        from <span className="text-xl font-semibold"> {music.ratings}&nbsp;</span>ratings</p>
                 </div>
             </div>
 
-            <p className="mt-2">Reviewed by: <a href="/#" className="font-bold text-sky-900">{review.username}</a></p>
-            <p className="mt-2">Review Date: {review.reviewDate}</p>
+            <p className="mt-2"><span className="font-semibold"> Reviewed by: </span> <a href="/#" className="font-bold text-sky-900">{review.username}</a></p>
+            <p className="mt-2"><span className="font-semibold">Review Date: </span>{review.reviewDate}</p>
             <p className="mt-2">{review.content}</p>
         </div>
     );
