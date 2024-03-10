589d751ee8e7430c776f4fa4365715f0fca1f338
spacemen0 <frakctures@outlook.com>
2024-03-10 23:07:29 +0100

    fix pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,10 @@
-import { useDispatch } from "react-redux";
 import { PageHeader } from "../layouts/PageHeader";
-import { setUser } from "../slices/userSlice";
 import { useNavigate } from "react-router-dom";
 
 export function Login() {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    const userData = {
-      name: "spacemen0",
-      id: "1",
-    };
-    console.log(userData);
-    dispatch(setUser(userData));
     navigate("/");
   };
   return (
