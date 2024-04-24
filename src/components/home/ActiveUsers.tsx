4d8d6224ec03f750788cf84f71b4960ddf62768d
spacemen0 <frakctures@outlook.com>
2024-04-25 01:12:23 +0200

    fix homepage SideList.tsx width, add CommentSection.tsx for MediaList and User profile, add file upload size limit, improve some styles

---
+++ src/components/home/ActiveUsers.tsx
@@ -0,0 +1,9 @@
+export function ActiveUsers() {
+  return (
+    <div className="mt-6 flex flex-col text-Neutral lg:w-10/12  lg:mt-12">
+      <h1 className="text-2xl font-bold text-Neutral xl:text-3xl">
+        Active Users
+      </h1>
+    </div>
+  );
+}
