25af9a63c6dafbcc4cbf9a2c5d26f692fb62a9b6
spacemen0 <frakctures@outlook.com>
2024-05-19 22:18:38 +0200

    lazy loading image

--- src/components/common/FullImage.tsx
+++ src/components/common/FullImage.tsx
@@ -1,5 +1,12 @@
 import { ImageProps } from "../../utils/type";
 
 export function FullImage({ src, alt }: ImageProps) {
-  return <img src={src} alt={alt} className="h-full w-full object-cover" />;
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className="h-full w-full object-cover"
+      loading={"lazy"}
+    />
+  );
 }
